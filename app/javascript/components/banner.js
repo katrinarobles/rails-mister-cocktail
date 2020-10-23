6ee4fca2c9eacb93350149faa14a7e4e2ce1cefe
Katrina Robles <katrinarobles@live.com>
2020-10-23 16:25:31 +0200

    active storage

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,9 +2,9 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["<h1>A lot can happen</h1>", "<h1>with just</h1>", " <h1><strong>one</strong> drink</h1>"],
-    typeSpeed: 100,
-    loop: true
+    strings: ["<h1>A lot can happen</h1>", "<h1>with just </h1>", " <h1><strong>one</strong></h1>", "<h1>Cocktail </h1>"],
+    typeSpeed: 200,
+    loop: false
   });
 }
 

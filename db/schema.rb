27ce36facd26bb7387aabfad47f1524672a4c515
Katrina Robles <katrinarobles@live.com>
2020-10-23 16:34:29 +0200

    chocalate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_100029) do
+ActiveRecord::Schema.define(version: 2020_10_22_142412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

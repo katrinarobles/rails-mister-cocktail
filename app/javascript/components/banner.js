5c456108167930cf318a5289077737164fef62fa
Katrina Robles <katrinarobles@live.com>
2021-03-05 18:26:00 +0100

    treid to restyle

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,9 +2,12 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["<h1>A lot can happen</h1>", "<h1>with just </h1>", " <h1><strong>one</strong></h1>", "<h1>Cocktail </h1>"],
-    typeSpeed: 200,
-    loop: false
+    strings: ["...", "<h1>A lot can happen</h1>" ,"<h1>with just <strong>one</strong> Cocktail </h1>"],
+    typeSpeed: 100,
+    loop: false,
+    showCursor: false,
+    // cursorChar: '|',
+    autoInsertCss: true
   });
 }
 
